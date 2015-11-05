5ea729af17950866071c6d70482a8c6a35aa52f9
Joe Wilm <joe@jwilm.com>
2015-11-05 13:29:05 -0800

    Start Iron server with handler stubs
    This commit provides the framework for handling HTTP requests. Except
    for /ping, none of the handlers are implemented. Try it!
    
    ```sh
    cargo build
    target/debug/racerd serve --secret-file=hi --port 3000
    ```
    
    In another terminal,
    
    ```sh
    curl localhost:3000/ping
    ```
    
    and a pong should be returned.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
 extern crate iron;
+#[macro_use]
 extern crate router;
 extern crate bodyparser;
 extern crate racer;
 extern crate crypto;
+
+use iron::{Iron, Chain};
 
 #[derive(Debug)]
 pub struct Config {
@@ -10,12 +13,49 @@ pub struct Config {
     pub secret_file: String
 }
 
-#[allow(unused_variables)]
 pub fn serve(config: Config) {
+    // For debugging. Torn about `log` crate since there's potentially a lot of noise from our
+    // dependencies.
     println!("{:?}", config);
-    unimplemented!();
+
+    let chain = Chain::new(router!(
+        post "/parse_file"  => handle::parse_file,
+        post "/completions" => handle::completions,
+        post "/definition"  => handle::definition,
+        post "/declaration" => handle::declaration,
+        get  "/ping"        => handle::ping));
+
+    let host = format!("localhost:{}", config.port);
+    Iron::new(chain).http(&host[..]).unwrap();
 }
 
-#[test]
-fn it_works() {
+#[allow(unused_variables)]
+mod handle {
+    use iron::prelude::*;
+    use iron::status;
+
+    /// Parse a file and return a list of issues (warnings, errors) encountered
+    pub fn parse_file(req: &mut Request) -> IronResult<Response> {
+        unimplemented!();
+    }
+
+    /// Given a location, return a list of possible completions
+    pub fn completions(req: &mut Request) -> IronResult<Response> {
+        unimplemented!();
+    }
+
+    /// Given a location, return where the identifier is defined
+    pub fn definition(req: &mut Request) -> IronResult<Response> {
+        unimplemented!();
+    }
+
+    /// Given a location, return a location where the identifier is declared
+    pub fn declaration(req: &mut Request) -> IronResult<Response> {
+        unimplemented!();
+    }
+
+    /// Check if the server is accepting requests
+    pub fn ping(req: &mut Request) -> IronResult<Response> {
+        Ok(Response::with((status::Ok, "{\"pong\": true}")))
+    }
 }

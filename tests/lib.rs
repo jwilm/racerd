a0b614ec3a76ec86dbe47d7dfe8c437af0359e62
Joe Wilm <joe@jwilm.com>
2015-11-17 08:18:41 -0800

    Add /find_definition test for multiple dirty files
    Checks that /find_definition correctly resolves cross file definitions
    when the buffers have not been written to disk.

--- tests/lib.rs
+++ tests/lib.rs
@@ -8,7 +8,9 @@ mod http {
 
     use rustc_serialize::json::{Json};
     use json_request::{request_str, Method};
+    use libracerd::util::fs::TmpFile;
 
+    /// Checks that /find_definition works within a single buffer
     #[test]
     fn find_definition() {
         http::with_server(|server| {
@@ -34,6 +36,50 @@ mod http {
                 "line": 1,
                 "column": 3,
                 "file_path": "src.rs"
+            })).unwrap();
+
+            // They should be equal
+            assert_eq!(actual, expected);
+        });
+    }
+
+    /// Checks that /find_definition correctly resolves cross file definitions when the buffers have
+    /// not been written to disk.
+    #[test]
+    fn find_definition_multiple_dirty_buffers() {
+
+        http::with_server(|server| {
+            // Build request args
+            let url = server.url("/find_definition");
+            let request_obj = stringify!({
+                "buffers": [{
+                    "file_path": "src.rs",
+                    "contents": "mod src2;\nuse src2::{foo,myfn};\nfn main() {\n    myfn();\n}\n"
+                }, {
+                    "file_path": "src2.rs",
+                    "contents": "\npub fn myfn()\npub fn foo() {}\n"
+                }],
+                "file_path": "src.rs",
+                "line": 4,
+                "column": 7
+            });
+
+            // Create an *empty* temporary file. The request contains unsaved file contents. For
+            // rust module inclusions to work, the compiler checks certain locations on the file
+            // system where a module file could be located. It simply doesn't work with unnamed
+            // files.
+            let _f = TmpFile::with_name("src2.rs", "");
+
+            // Make request
+            let res = request_str(Method::Post, &url[..], Some(request_obj)).unwrap().unwrap();
+
+            // Build actual/expected objects
+            let actual = Json::from_str(&res[..]).unwrap();
+            let expected = Json::from_str(stringify!({
+                "text": "myfn",
+                "line": 2,
+                "column": 7,
+                "file_path": "src2.rs"
             })).unwrap();
 
             // They should be equal

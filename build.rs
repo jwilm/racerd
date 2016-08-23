a8d17166f1a98d74597b0c067f59bbd72cf9c47b
Ralph Giles <giles@mozilla.com>
2016-08-23 08:46:01 -0700

    Invert build.rs expect() messages.
    Result::expect() is oddly named. It takes a panic string as an
    argument, and one wants to explain what was expected there so
    the code reads like an English sentence. However, the message
    is printed on error, when what is expected _isn't_ present:
    
      thread 'main' panicked at 'OUT_DIR environment variable is set',
      ../src/libcore/option.rs:699
    
    This is confusing as an error message.

--- build.rs
+++ build.rs
@@ -5,14 +5,14 @@ use std::fs;
 use std::path::Path;
 
 fn serde_expand(infile: &str, outfile: &str) {
-    let out_dir = env::var_os("OUT_DIR").expect("OUT_DIR environment var is set");
+    let out_dir = env::var_os("OUT_DIR").expect("OUT_DIR not set in the environment");
     let src = Path::new(infile);
     let dst = Path::new(&out_dir).join(outfile);
 
     // Make output directory
     fs::create_dir(dst.parent().unwrap()).ok();
 
-    serde_codegen::expand(&src, &dst).expect("codegen expand successfully");
+    serde_codegen::expand(&src, &dst).expect("codegen failed");
 }
 
 pub fn main() {
@@ -20,4 +20,3 @@ pub fn main() {
     serde_expand("src/http/definition.rs.in", "http/definition.rs");
     serde_expand("src/engine/mod.rs.in", "engine/mod.rs");
 }
-
